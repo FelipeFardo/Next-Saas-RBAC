6b1bde54eabbdc86d5aa04864309823ed93a154e
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-05-22 01:44:10 -0300

    feat: setting app permissions

--- packages/auth/src/models/project.ts
+++ packages/auth/src/models/project.ts
@@ -3,6 +3,6 @@ import { z } from 'zod'
 export const projectSchema = z.object({
   __typename: z.literal('Project').default('Project'),
   id: z.string(),
-  ownderId: z.string(),
+  ownerId: z.string(),
 })
 export type Project = z.infer<typeof projectSchema>
