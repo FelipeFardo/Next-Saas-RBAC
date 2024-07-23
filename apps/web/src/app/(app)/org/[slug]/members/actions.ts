3c05b55bd0407e9004efa64523afc23f6400dce2
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-07-23 19:18:04 -0300

    feat: update member role

--- apps/web/src/app/(app)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -1,9 +1,11 @@
 'use server'
 
+import type { Role } from '@saas/auth'
 import { revalidateTag } from 'next/cache'
 
 import { getCurrentOrg } from '@/auth/auth'
 import { removeMember } from '@/http/remove-member'
+import { updateMember } from '@/http/update-member'
 
 export async function removeMemberAction(memberId: string) {
   const currentOrg = getCurrentOrg()
@@ -12,6 +14,19 @@ export async function removeMemberAction(memberId: string) {
     await removeMember({
       org: currentOrg!,
       memberId,
+    })
+    revalidateTag(`${currentOrg}/members`)
+  } catch (error) {}
+}
+
+export async function updateMemberAction(memberId: string, role: Role) {
+  const currentOrg = getCurrentOrg()
+
+  try {
+    await updateMember({
+      org: currentOrg!,
+      memberId,
+      role,
     })
     revalidateTag(`${currentOrg}/members`)
   } catch (error) {}
